afb1455f7a66c6eed0c537c2631bae1de969702a
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-13 10:33:51 -0400

    Initial Commit

---
+++ Lesson8Output/Lesson8OutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson8OutputApp.swift
+//  Lesson8Output
+//
+//  Created by Amulya on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Lesson8OutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
